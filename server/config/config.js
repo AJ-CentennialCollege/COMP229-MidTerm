7e2d113577808d12d2f54cb7250399f5d49d157d
AJ-CentennialCollege <abalbaca@my.centennialcollege.ca>
2022-10-27 01:00:52 -0400

    Added Mongo URI and Fix Add, Edit and Delete

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,4 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+//export const MongoURI = "mongodb://localhost:27017/media";
 export const Secret = "someSecret";
+export const MongoURI = "mongodb+srv://midterm:midterm@comp229-ajbalbacal.bs01ys8.mongodb.net/?retryWrites=true&w=majority"
+
